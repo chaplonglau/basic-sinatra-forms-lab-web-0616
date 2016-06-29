ef93f51703e6dc267ba90e8909901c5d34acbc72
chaplonglau <chaplonglau@gmail.com>
2016-06-29 07:46:10 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,15 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+
+    erb :newteam
+
+  end
+
+  post '/team' do
+    @name, @coach, @pg, @sg, @pf, @sf, @c = params.values
+    erb :team
+  end
 
 end
